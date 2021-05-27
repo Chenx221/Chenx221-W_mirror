588e499d40c61329dc6c4b46f0c66ca70d2632ed
Chenx221 <chenx221@yandex.com>
2021-05-27 06:04:14 +0000

    upload
    尚未完成

---
+++ 实验5/5.1.cpp
@@ -0,0 +1,20 @@
+/*
+	Name:实验5-1 采用邻接矩阵表示法创建无向网并输出和统计各个结点的度
+*/
+
+//#include"邻接矩阵.h" //UTF-8下貌似找不到头文件（乱码 
+#include"ljjz.h"
+int main(){
+	cout << "****采用邻接矩阵表示法创建无向网****" << endl ;
+	AMGraph G;
+	CreateUDN(G);
+	cout <<endl;
+	cout << "该图的邻接矩阵如下：" << endl;
+  	PrtAMGraph(G);
+	cout << endl<<"*****各个顶点的度：*****" << endl;
+	for (int i=0;i<G.vexnum;i++)
+	{
+		printf("第%d个顶点 %c 的度：%d\n",i,G.vexs[i],Degree(G,i));
+	}
+	return 0;
+}//main
