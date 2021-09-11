dfa0303630f32592b0e175922cb08cd2ed57c7f7
Chenx221 <chenx221@yandex.com>
2021-09-11 18:02:03 +0800

    update
    2021.9.11

---
+++ JavaLearnPratice/src/p/P594.java
@@ -0,0 +1,12 @@
+package p;
+
+public class P594 {
+
+	public static void main(String[] args) {
+		StringBuilder builder = new StringBuilder("TEXT: ");
+		for (int i = 1; i <= 10; i++)
+			builder.append(i);
+		System.out.println(builder.toString());
+	}
+
+}
