52178f8386e0eabee1b19a509f79e3b5d8256b6e
Chenx221 <chenx221@yandex.com>
2021-11-03 10:44:30 +0800

    Update sy3.java

--- JavaCode/src/cyou/chenx221/work1103/sy3.java
+++ JavaCode/src/cyou/chenx221/work1103/sy3.java
@@ -21,23 +21,26 @@ class Rectangle implements Figure {
     }
 }
 
-class Square implements Figure{
+class Square implements Figure {
     private double side;
 
     public void setSide(double side) {
         this.side = side;
     }
 
-    public void Area(){
-        System.out.println("正方形的面积是"+Math.pow(side,2));
+    public void Area() {
+        System.out.println("正方形的面积是" + Math.pow(side, 2));
     }
 }
+
 public class sy3 {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle();
-        r1.setLength(10.0);r1.setWidth(20.0);
+        r1.setLength(10.0);
+        r1.setWidth(20.0);
         Square s1 = new Square();
         s1.setSide(5);
-        r1.Area();s1.Area();
+        r1.Area();
+        s1.Area();
     }
 }
