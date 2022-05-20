a7edd6219401332c008fe0a19141018a05f88f62
Chenx221 <chenx221@yandex.com>
2022-05-20 11:30:12 +0800

    update

---
+++ work0520/js/main.js
@@ -0,0 +1,16 @@
+function click_username(){
+    document.getElementById("username").style.backgroundColor = "rgba(255,255,255,0.5)";
+}
+function leave_username(){
+    document.getElementById("username").style.backgroundColor = "rgba(255,255,255,0)";
+}
+function click_password(){
+    document.getElementById("password").style.backgroundColor = "rgba(255,255,255,0.5)";
+}
+function leave_password(){
+    document.getElementById("password").style.backgroundColor = "rgba(255,255,255,0)";
+}
+function login1(){
+    alert("登录失败，请重试");
+    debugger;
+}
